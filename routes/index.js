e0f09ec3fad59176a3ff4fc247dbb45438696877
Aaron Tumbokon <tumbokon.aaronjulius@outlook.com>
2023-10-29 00:29:49 -0600

    auto redirect to google auth

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,10 @@ const express = require('express');
 const passport = require('passport');
 const router = express.Router();
 
+router.get('/', (req, res) => {
+    res.redirect('/auth/google');
+});
+
 router.get('/auth/google',
     passport.authenticate('google', { scope: ['profile', 'email'] })
 );
--- utils/passport.js
+++ utils/passport.js
@@ -15,7 +15,7 @@ passport.deserializeUser((user, done) => {
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:8080/auth/google/callback"
+    callbackURL: "https://personal-project-fdrz.onrender.com/auth/google/callback"
 },
 (accessToken, refreshToken, profile, done) => {
 return done(null, profile);
