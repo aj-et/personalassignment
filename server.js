777c75d756f2668f13765e35d09c46e6c6d6ce9d
Aaron Tumbokon <tumbokon.aaronjulius@outlook.com>
2023-11-04 21:43:07 -0600

    updated server.js

--- server.js
+++ server.js
@@ -7,30 +7,21 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT;
 
-app.use(cors({
-    origin: function (origin, callback) {
-        // Allow requests from localhost
-        if (!origin || origin === 'https://personal-project-fdrz.onrender.com' || origin === 'http://localhost:8080') {
-            callback(null, true);
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    }
-}));
+app.use(cors());
 
 app.use(express.json()); // This will handle JSON requests
 
 app.use('/', require('./routes/index'))
 app.use('/users', require('./routes/users'));
-app.use('/auth', require('./routes/auth'));
+// app.use('/auth', require('./routes/auth'));
 
-app.get('/register', (req, res) => {
-    res.sendFile(__dirname + '/public/index.html');
-});
+// app.get('/register', (req, res) => {
+//     res.sendFile(__dirname + '/public/index.html');
+// });
 
-app.get('/login', (req, res) => {
-    res.sendFile(__dirname + '/public/login.html');
-});
+// app.get('/login', (req, res) => {
+//     res.sendFile(__dirname + '/public/login.html');
+// });
 
 mongodb.run()
 
